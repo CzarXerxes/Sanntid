19a9087b19f7989cb1d1eaa140fb81ff3a662bc8
andershanssen <andershanssen92@gmail.com>
2016-03-02 14:30:21 +0100

    Create UserIO.go

---
+++ Hovedprosjekt/UserIO.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"driver"
+	"fmt"
+)
+
+type order struct {
+	orderType int //Obstruction = -1, Stop = 0, External order = 1, Internal order = 2
+	floor     int
+	direction bool //Up = True, down = false
+}
+
+func init_userIO() {
+	driver.Io_init()
+}
+
+func receive_order() order {
+}
+
+func main() {
+	init_userIO()
+	for {
+		newOrder := receive_order()
+	}
+}
