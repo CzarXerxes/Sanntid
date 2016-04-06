166945e5f55d48acb5295906caa091d9a9c3847e
CzarXerxes <nils_93@hotmail.co.uk>
2016-04-06 18:54:59 +0200

    Update UserModule.go

--- Hovedprosjekt/src/user/UserModule.go
+++ Hovedprosjekt/src/user/UserModule.go
@@ -3,21 +3,23 @@ package user
 import (
 	"driver"
 	//"fmt"
+	"time"
 )
 
 type ElevatorOrder struct {
 	OrderType driver.Elev_button_type_t //Down order = 0 Up order = 1, Internal order = 2
-	Floor     int                       //1 indexed(Floor 1 = 1, Floor 2 = 2 ...)
+	Floor     int                       //0 indexed(Floor 1 = 0, Floor 2 = 1 ...)
 }
 
-func init_userIO() {
+func userModuleInit() {
 	driver.Elev_init()
 }
 
-func receive_order() ElevatorOrder {
+func receiveOrder() ElevatorOrder {
 	var tempOrder ElevatorOrder
 	var orderReceived bool = false
 	for {
+		time.Sleep(time.Millisecond * 10)
 		for i := 0; i < driver.N_FLOORS; i++ {
 			for j := 0; j < driver.N_BUTTONS; j++ {
 				if driver.Elev_get_button_signal(driver.Elev_button_type_t(j), i) == 1 {
@@ -35,9 +37,9 @@ func receive_order() ElevatorOrder {
 }
 
 func Run(c chan ElevatorOrder) {
-	init_userIO()
+	userModuleInit()
 	for {
-		newOrder := receive_order()
+		newOrder := receiveOrder()
 		c <- newOrder
 	}
 }
