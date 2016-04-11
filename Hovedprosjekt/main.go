ff0e80d93b8d6621f9012f173ac392591927d1ad
CzarXerxes <nils_93@hotmail.co.uk>
2016-04-11 18:36:14 +0200

    Update main.go

--- Hovedprosjekt/main.go
+++ Hovedprosjekt/main.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-	//"control"
+	"control"
 	//"driver"
-	//"elevator"
+	"elevator"
 	"fmt"
-	"runtime"
-	//"user"
 	"network"
+	"runtime"
+	"user"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	done := make(chan bool)
-	/*
-		UserToControlChan := make(chan user.ElevatorOrder)
-		ElevatorToControlChan := make(chan map[int]control.ElevatorNode)
-		ControlToElevatorChan := make(chan map[int]control.ElevatorNode)
+	UserToControlChan := make(chan user.ElevatorOrder)
+	ElevatorToControlChan := make(chan map[string]control.ElevatorNode)
+	ControlToElevatorChan := make(chan map[string]control.ElevatorNode)
+	NetworkToControlChan := make(chan map[string]control.ElevatorNode)
+	ControlToNetworkChan := make(chan map[string]control.ElevatorNode)
+	InitializeAddressChan := make(chan string)
 
-		go control.Run(ControlToElevatorChan, ElevatorToControlChan, UserToControlChan)
-		go elevator.Run(ElevatorToControlChan, ControlToElevatorChan)
-		go user.Run(UserToControlChan)
-	*/
-	go network.Run()
+	go control.Run(InitializeAddressChan, ControlToNetworkChan, NetworkToControlChan, ControlToElevatorChan, ElevatorToControlChan, UserToControlChan)
+	go elevator.Run(ElevatorToControlChan, ControlToElevatorChan)
+	go user.Run(UserToControlChan)
+	go network.Run(InitializeAddressChan, NetworkToControlChan, ControlToNetworkChan)
 	<-done
 	fmt.Println("Done")
 }
