9400d1d1c732751c7db9e459ebcddb324e7ca912
CzarXerxes <nils_93@hotmail.co.uk>
2016-04-16 08:03:00 +0200

    Update main.go

--- Hovedprosjekt/main.go
+++ Hovedprosjekt/main.go
@@ -14,6 +14,8 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	done := make(chan bool)
+	blockUserChan := make(chan bool)
+	blockNetworkChan := make(chan bool)
 	UserToControlChan := make(chan user.ElevatorOrder)
 	ElevatorToControlChan := make(chan map[string]control.ElevatorNode)
 	ControlToElevatorChan := make(chan map[string]control.ElevatorNode)
@@ -21,10 +23,10 @@ func main() {
 	ControlToNetworkChan := make(chan map[string]control.ElevatorNode)
 	InitializeAddressChan := make(chan string)
 
-	go control.Run(InitializeAddressChan, ControlToNetworkChan, NetworkToControlChan, ControlToElevatorChan, ElevatorToControlChan, UserToControlChan)
+	go control.Run(InitializeAddressChan, blockUserChan, blockNetworkChan, ControlToNetworkChan, NetworkToControlChan, ControlToElevatorChan, ElevatorToControlChan, UserToControlChan)
 	go elevator.Run(ElevatorToControlChan, ControlToElevatorChan)
-	go user.Run(UserToControlChan)
-	go network.Run(InitializeAddressChan, NetworkToControlChan, ControlToNetworkChan)
+	go user.Run(blockUserChan, UserToControlChan)
+	go network.Run(InitializeAddressChan, blockNetworkChan, NetworkToControlChan, ControlToNetworkChan)
 	<-done
 	fmt.Println("Done")
 }
