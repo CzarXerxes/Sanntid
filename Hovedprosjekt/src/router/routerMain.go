7756391a7c6bb11402b2514da6640dcb13948d83
CzarXerxes <nils_93@hotmail.co.uk>
2016-04-17 23:48:05 +0200

    Update routerMain.go

--- Hovedprosjekt/src/router/routerMain.go
+++ Hovedprosjekt/src/router/routerMain.go
@@ -2,51 +2,37 @@ package router
 
 import (
 	"control"
-	"encoding/gob"
-	"reflect"
-	"time"
+	"sync"
+	"driver"
+	"net"
+	"strings"
 )
 
-var elevatorEncoders = make(map[string]*gob.Encoder)
-var elevatorDecoders = make(map[string]*gob.Decoder)
+var routerIPAddress string
 
-var orderMapInTransit = make(map[string]control.ElevatorNode)
-var elevatorWhichSentTheOrder string
-var shouldSendOrderMap bool
+var elevatorWhichSentTheOrderMutex = &sync.Mutex{}
+var connectionMutex = &sync.Mutex{}
 
-func receiveNewElevatorStatus(dec *gob.Decoder, channel chan map[string]control.ElevatorNode) {
-	var newMap = make(map[string]control.ElevatorNode)
-	for {
-		dec.Decode(&newMap)
-		channel <- newMap
-	}
+func getRouterIP() string{
+	return driver.IP
 }
 
-func getOrderMapThread(channel chan map[string]control.ElevatorNode) {
-	for {
-		time.Sleep(time.Millisecond * 10)
-		tempOrderMap := <-channel
-		if !reflect.DeepEqual(orderMapInTransit, tempOrderMap) {
-			connectionMutex.Lock()
-			control.CopyMapByValue(tempOrderMap, orderMapInTransit)
-			connectionMutex.Unlock()
-			shouldSendOrderMap = true
-		}
-	}
+func routerModuleInit() {
+	routerIPAddress = getRouterIP()
+	portString := []string{":", driver.Port}
+	elevatorSocketListener, _ = net.Listen("tcp", strings.Join(portString, ""))
 }
 
-func sendOrderMapThread() {
-	var tempOrderMap = make(map[string]control.ElevatorNode)
-	for {
-		time.Sleep(time.Millisecond * 10)
-		connectionMutex.Lock()
-		control.CopyMapByValue(orderMapInTransit, tempOrderMap)
-		connectionMutex.Unlock()
-		if shouldSendOrderMap {
-			for elevator, _ := range elevatorAliveConnectionsMap {
-				elevatorEncoders[elevator].Encode(tempOrderMap)
-			}
-		}
-		shouldSendOrderMap = false
-	}
+
+func Run(){
+	elevatorChannel := make(chan map[string]control.ElevatorNode)
+	wg := new(sync.WaitGroup)
+	wg.Add(5)
+	routerModuleInit()
+	go connectNewElevatorsThread(wg, elevatorChannel)
+	go checkElevatorStillConnectedThread()
+	go tellElevatorStillConnectedThread()
+	go getOrderMapThread(elevatorChannel)
+	go sendOrderMapThread()
+	wg.Wait()
 }
