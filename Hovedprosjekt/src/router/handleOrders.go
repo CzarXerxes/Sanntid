03d7ea07a91901f53defbe4dada239fbd6897997
CzarXerxes <nils_93@hotmail.co.uk>
2016-04-17 20:20:31 +0200

    Create handleOrders.go

---
+++ Hovedprosjekt/src/router/handleOrders.go
@@ -0,0 +1,64 @@
+package router
+
+import (
+	"control"
+	"encoding/gob"
+	//"encoding/json"
+	"fmt"
+	"net"
+	"os/exec"
+	"reflect"
+	"sync"
+	"time"
+)
+
+
+
+
+
+
+var elevatorEncoders = make(map[string]*gob.Encoder)
+var elevatorDecoders = make(map[string]*gob.Decoder)
+
+var matrixInTransit = make(map[string]control.ElevatorNode)
+var elevatorWhichSentTheOrder string
+
+
+
+func receiveIncoming(dec *gob.Decoder, channel chan map[string]control.ElevatorNode) {
+	var newMap = make(map[string]control.ElevatorNode)
+	for {
+		dec.Decode(&newMap)
+		channel <- newMap
+	}
+}
+
+
+func getMatrixThread(channel chan map[string]control.ElevatorNode) {
+	for {
+		time.Sleep(time.Millisecond * 10)
+		tempMatrix := <-channel
+		if !reflect.DeepEqual(matrixInTransit, tempMatrix) {
+			connectionMutex.Lock()
+			copyMapByValue(tempMatrix, matrixInTransit)
+			connectionMutex.Unlock()
+			sendMatrix = true
+		}
+	}
+}
+
+func sendMatrixThread() {
+	var tempMatrix = make(map[string]control.ElevatorNode)
+	for {
+		time.Sleep(time.Millisecond * 10)
+		connectionMutex.Lock()
+		copyMapByValue(matrixInTransit, tempMatrix)
+		connectionMutex.Unlock()
+		if sendMatrix {
+			for elevator, _ := range elevatorAliveConnections {
+				elevatorEncoders[elevator].Encode(tempMatrix)
+			}
+		}
+		sendMatrix = false
+	}
+}
