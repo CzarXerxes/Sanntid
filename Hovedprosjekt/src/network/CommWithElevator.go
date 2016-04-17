0eca9e80b805e4e817dd04f8e2b9a164fe2f5b88
andershanssen <andershanssen92@gmail.com>
2016-04-17 20:28:31 +0200

    Update and rename ElevatorCommunication.go to CommWithElevator.go

---
+++ Hovedprosjekt/src/network/CommWithElevator.go
@@ -0,0 +1,48 @@
+package network
+
+import(
+	"driver"
+	"control"
+	"reflect"
+	"time"
+	"sync"
+)
+
+func sendInitialAddressToElevator(address string, initializeAddressChannel chan string) {
+	initializeAddressChannel <- address
+}
+
+func communicateWithElevatorThread(sendChannel chan map[string]control.ElevatorNode, receiveChannel chan map[string]control.ElevatorNode) {
+	go receiveFromElevatorThread(receiveChannel)
+	go sendToElevatorThread(sendChannel)
+}
+
+func receiveFromElevatorThread(receiveChannel chan map[string]control.ElevatorNode) {
+	var tempMatrix = make(map[string]control.ElevatorNode)
+	for {
+		time.Sleep(time.Millisecond * 10)
+		if !sendMatrixToElevator {
+			tempMatrix = <-receiveChannel
+			if !reflect.DeepEqual(matrixInTransit, tempMatrix) {
+				elevatorMatrixMutex.Lock()
+				copyMapByValue(tempMatrix, matrixInTransit)
+				elevatorMatrixMutex.Unlock()
+				sendMatrixToRouter = true
+			}
+		}
+	}
+}
+
+func sendToElevatorThread(sendChannel chan map[string]control.ElevatorNode) {
+	var tempMatrix = make(map[string]control.ElevatorNode)
+	for {
+		time.Sleep(time.Millisecond * 10)
+		if sendMatrixToElevator {
+			elevatorMatrixMutex.Lock()
+			copyMapByValue(matrixInTransit, tempMatrix)
+			elevatorMatrixMutex.Unlock()
+			sendChannel <- tempMatrix
+			sendMatrixToElevator = false
+		}
+	}
+}
