d0db4f9a4cf7c2953663002a9c64c5261fa70862
andershanssen <andershanssen92@gmail.com>
2016-04-17 18:48:13 +0200

    Update ElevatorCommunication.go

--- Hovedprosjekt/src/elevator/ElevatorCommunication.go
+++ Hovedprosjekt/src/elevator/ElevatorCommunication.go
@@ -1,1 +1,64 @@
+package elevator
+
+import(
+)
+
+func communicationThread(sendChannel chan map[string]control.ElevatorNode, receiveChannel chan map[string]control.ElevatorNode) {
+	go receiveNewMatrix(receiveChannel)
+	go sendNewMatrix(sendChannel)
+}
+
+func receiveNewMatrix(receiveChannel chan map[string]control.ElevatorNode) {
+	var emptyMatrix = make(map[string]control.ElevatorNode)
+	var tempMatrix = make(map[string]control.ElevatorNode)
+	for {
+		time.Sleep(time.Millisecond * 10)
+		tempMatrix = <-receiveChannel
+		elevatorMatrixMutex.Lock()
+		if !reflect.DeepEqual(emptyMatrix, tempMatrix) {
+			if !reflect.DeepEqual(matrixBeingHandled, tempMatrix) {
+				copyMapByValue(tempMatrix, elevatorMatrix)
+				copyMapByValue(tempMatrix, matrixBeingHandled)
+				orderArray = createOrderArray()
+				tempOrder := tempMatrix[control.LocalAddress]
+				Save(backupOrderFilePath, tempOrder)
+				//Load(backupOrderFilePath, tempOrder)
+				//fmt.Println("Printing on receive thread")
+				//fmt.Println(tempOrder)
+				//Check(err)
+			}
+		}
+		if receivedFirstMatrix == false {
+			receivedFirstMatrix = true
+		}
+		elevatorMatrixMutex.Unlock()
+	}
+}
+
+func sendNewMatrix(sendChannel chan map[string]control.ElevatorNode) {
+	var emptyMatrix = make(map[string]control.ElevatorNode)
+	var tempMatrix = make(map[string]control.ElevatorNode)
+	for {
+		time.Sleep(time.Millisecond * 10)
+		elevatorMatrixMutex.Lock()
+		if openSendChan {
+			copyMapByValue(elevatorMatrix, tempMatrix)
+			if !reflect.DeepEqual(emptyMatrix, tempMatrix) {
+				if !reflect.DeepEqual(matrixBeingHandled, tempMatrix) {
+					//fmt.Println("A completed order was sent")
+					sendChannel <- tempMatrix
+					tempOrder := tempMatrix[control.LocalAddress]
+					Save(backupOrderFilePath, tempOrder)
+					//Load(backupOrderFilePath, tempOrder)
+					//fmt.Println("Printing on send thread")
+					//fmt.Println(tempOrder)
+					//Check(err)
+					copyMapByValue(tempMatrix, matrixBeingHandled)
+				}
+			}
+			openSendChan = false
+		}
+		elevatorMatrixMutex.Unlock()
+	}
+}
 
