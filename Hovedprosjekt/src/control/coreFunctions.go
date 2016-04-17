325876f9ece568ab9301b8cff359724529901df8
CzarXerxes <nils_93@hotmail.co.uk>
2016-04-17 21:07:57 +0200

    Update coreFunctions.go

--- Hovedprosjekt/src/control/coreFunctions.go
+++ Hovedprosjekt/src/control/coreFunctions.go
@@ -2,10 +2,7 @@ package control
 
 import (
 	"driver"
-	"encoding/gob"
-	"os"
 	"sync"
-	"time"
 	"user"
 )
 
@@ -24,7 +21,7 @@ type ElevatorNode struct {
 	CurrentOrders    [driver.N_BUTTONS][driver.N_FLOORS]bool
 }
 
-func sendUpdatedMatrix() { //Sends updated map of elevators to network
+func sendUpdatedMatrix() {
 	openSendChanElevator = true
 	if !elevatorIsOffline {
 		openSendChanNetwork = true
@@ -47,7 +44,7 @@ func completePreCrashOrders(orders *ElevatorNode, sendChannel chan map[string]El
 }
 
 func controlInit(initializeAddressChannel chan string, blockUserChannel chan bool, blockNetworkChannel chan bool, sendNetworkChannel chan map[string]ElevatorNode, receiveNetworkChannel chan map[string]ElevatorNode, sendElevatorChannel chan map[string]ElevatorNode, receiveElevatorChannel chan map[string]ElevatorNode) {
-	driver.Elev_init() //Initialize hardware
+	driver.Elev_init() 
 	var tempMatrix = make(map[string]ElevatorNode)
 	elevatorMatrix = make(map[string]ElevatorNode)
 
@@ -77,37 +74,24 @@ func controlInit(initializeAddressChannel chan string, blockUserChannel chan boo
 }
 
 func distributeOrder(localElevAddress string, newOrder user.ElevatorOrder, elevatorMatrix map[string]ElevatorNode) {
-	/*
-		for elev, node := range elevatorMatrix {
-			fmt.Println(elev)
-			fmt.Println(node.CurrentFloor)
-		}
-	*/
 	var tempMatrix = make(map[string]ElevatorNode)
-	var bestElevAddress string = localElevAddress //Variable to store best elevator for new order. By default assume initially this is the local elevator
+	var bestElevAddress string = localElevAddress
 	if newOrder.OrderType == driver.BUTTON_COMMAND {
-		//fmt.Println("The order was internal")
 		goto ReturnElevator
 	} else if newOrder.OrderType == driver.BUTTON_CALL_UP {
-		//fmt.Println("The order was up button")
-		//Special case: check if any elevators on ordered floor are going upwards
 		for address, elevator := range elevatorMatrix {
-			//fmt.Println("Checking if any elevator is on same floor as order")
 			if elevator.CurrentFloor == newOrder.Floor && elevator.CurrentDirection == driver.DIRN_UP {
 				bestElevAddress = address
 				goto ReturnElevator
 			}
 		}
-		//fmt.Println("Didn't find any elevators on same floor")
 		for i := newOrder.Floor; i >= 0; i-- {
-			//fmt.Println("Checking if there are any empty elevators on floor under the order")
 			for address, elevator := range elevatorMatrix {
 				if elevator.CurrentFloor == i && ordersEmpty(elevator) {
 					bestElevAddress = address
 					goto ReturnElevator
 				}
 			}
-			//fmt.Println("Checking if there are any elevators going up on floor under the order")
 			for address, elevator := range elevatorMatrix {
 				if elevator.CurrentFloor == i && elevator.CurrentDirection == driver.DIRN_UP {
 					bestElevAddress = address
@@ -124,8 +108,6 @@ func distributeOrder(localElevAddress string, newOrder user.ElevatorOrder, eleva
 			}
 		}
 	} else if newOrder.OrderType == driver.BUTTON_CALL_DOWN {
-		//fmt.Println("The order was down button")
-		//Special case: check if any elevators on ordered floor are going downwards
 		for address, elevator := range elevatorMatrix {
 			if elevator.CurrentFloor == newOrder.Floor && elevator.CurrentDirection == driver.DIRN_DOWN {
 				bestElevAddress = address
@@ -165,8 +147,6 @@ ReturnElevator:
 	elevatorMatrixMutex.Unlock()
 }
 
-
-
 //The application was reliant on being able to copy maps by value, and as ElevatorNode is defined
 //in the control module, copyMapByValue() is also defined in the control module
 func CopyMapByValue(originalMap map[string]ElevatorNode, newMap map[string]ElevatorNode) {
@@ -178,17 +158,14 @@ func CopyMapByValue(originalMap map[string]ElevatorNode, newMap map[string]Eleva
 	}
 }
 
-
 func Run(initializeAddressChannel chan string, blockUserChannel chan bool, blockElevatorChannel chan bool, sendNetworkChannel chan map[string]ElevatorNode, receiveNetworkChannel chan map[string]ElevatorNode, sendElevatorChannel chan map[string]ElevatorNode, receiveElevatorChannel chan map[string]ElevatorNode, receiveUserChannel chan user.ElevatorOrder) {
 	wg := new(sync.WaitGroup)
 	wg.Add(4)
 
 	controlInit(initializeAddressChannel, blockUserChannel, blockElevatorChannel, sendNetworkChannel, receiveNetworkChannel, sendElevatorChannel, receiveElevatorChannel)
-
 	go networkThread(sendNetworkChannel, receiveNetworkChannel)
 	go userThread(receiveUserChannel)
 	go elevatorThread(sendElevatorChannel, receiveElevatorChannel)
 	go checkConnectedThread(initializeAddressChannel, sendNetworkChannel, receiveNetworkChannel)
-	
 	wg.Wait()
 }
